fd9cde8a38ffe20802ded14c4fb4edaca415f9c8
Jason Mavandi <Jason.Mavandi@emc.com>
2017-03-25 18:03:02 -0600

    Get the country if the IP is known

--- project/cf/public/examples/locate/Locate.php
+++ project/cf/public/examples/locate/Locate.php
@@ -1,9 +1,15 @@
 <?php
 
+
 class Locate
 {
-    public function find()
+    public function find($ip = '40.131.216.250')
     {
-        return 'Hello';
+        $apiky = '1fa8001dbc068cfed83fb2ec2c7773dc8eba6f5d';
+        $xml = file_get_contents('http://api.db-ip.com/addrinfo?api_key='.$apiky.'&addr='.$ip);
+        $json = json_decode($xml);
+        $country = $json->{'country'};
+
+        return $country;
     }
 }
