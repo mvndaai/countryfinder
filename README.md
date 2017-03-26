230b37cfecd856afa3f649771e0e0c0b4518bca8
Jason Mavandi <Jason.Mavandi@emc.com>
2017-03-25 20:29:12 -0600

    Update readme

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # countryfinder
 This is a client server setup that tells were a request comes from.
 
-
 ## Start the server
 ```
 docker-compose up server
@@ -9,3 +8,20 @@ docker-compose up server
 
 ## Go the the page
 http://localhost/restler/public/locate/index.php/locate/find
+
+# Issue
+
+* The client IP should be found in PHP using `$_SERVER['REMOTE_ADDR']`
+* Docker uses a proxy so the address in `$_SERVER['REMOTE_ADDR']` is from the proxy not the client
+* A solution should be use "net:host" to skip the proxy.
+  * Port 80 is not available when using "net:host"
+  * https://github.com/docker/docker/issues/15086
+
+# Partial Completion
+If you know the an IP you can pass it to the url.
+
+Example: This link will return "Peru"
+http://localhost/restler/public/locate/index.php/locate/find?ip=132.157.0.0
+
+And this link "Romania"
+http://localhost/restler/public/locate/index.php/locate/find?ip=5.2.128.0
--- project/cf/public/locate/Locate.php
+++ project/cf/public/locate/Locate.php
@@ -1,10 +1,19 @@
 <?php
 
-
 class Locate
 {
-    public function find($ip = '40.131.216.0')
+    public function find($ip = '')
     {
+        if (strlen($ip) == 0) {
+            $dockerIPprefix = '172.';
+            if (substr($_SERVER['REMOTE_ADDR'], 0, strlen($dockerIPprefix)) === $dockerIPprefix) {
+                $ip = '40.131.216.0';
+                print_r('IP is from Docker proxy, not client. Defaulting to '.$ip."\n");
+            } else {
+                $ip = $_SERVER['REMOTE_ADDR'];
+            }
+        }
+
         $apiky = '1fa8001dbc068cfed83fb2ec2c7773dc8eba6f5d';
         $xml = file_get_contents('http://api.db-ip.com/addrinfo?api_key='.$apiky.'&addr='.$ip);
         $json = json_decode($xml);
@@ -13,11 +22,11 @@ class Locate
         $countryDetails = file_get_contents('https://restcountries.eu/rest/v2/alpha?codes='.$countryCode);
         $country = json_decode($countryDetails)[0]->{'name'};
 
-        print_r($_SERVER);
-        print_r(getallheaders());
-        print_r(apache_response_headers());
+        // print_r($_SERVER);
+        // print_r($_SERVER['REMOTE_ADDR']);
+        // print_r(getallheaders());
+        // print_r(apache_response_headers());
 
         return $country;
-        //return $_SERVER['REMOTE_ADDR'];
     }
 }
